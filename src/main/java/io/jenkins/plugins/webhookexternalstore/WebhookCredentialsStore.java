219aed10ca86f3eb13e35ee8b1e4c796dc6979f4
Valentin Delaye <jonesbusy@users.noreply.github.com>
2026-02-11 06:54:27 +0100

    Add persistence for Webhook store

--- src/main/java/io/jenkins/plugins/webhookexternalstore/WebhookCredentialsStore.java
+++ src/main/java/io/jenkins/plugins/webhookexternalstore/WebhookCredentialsStore.java
@@ -4,16 +4,22 @@ import com.cloudbees.plugins.credentials.Credentials;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.CredentialsStore;
 import com.cloudbees.plugins.credentials.CredentialsStoreAction;
+import com.cloudbees.plugins.credentials.common.IdCredentials;
 import com.cloudbees.plugins.credentials.domains.Domain;
 import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.XmlFile;
 import hudson.model.ModelObject;
 import hudson.security.Permission;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import jenkins.model.Jenkins;
 import org.kohsuke.stapler.export.ExportedBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 
 /**
@@ -22,27 +28,40 @@ import org.springframework.security.core.Authentication;
 public class WebhookCredentialsStore extends CredentialsStore {
 
     /**
+     * Logger instance for this class.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(WebhookCredentialsStore.class.getName());
+
+    /**
      * The provider.
      */
-    private final WebhookCredentialsProvider provider;
+    private final transient WebhookCredentialsProvider provider;
 
     /**
      * The store action.
      */
-    private final WebhookCredentialsStoreAction action = new WebhookCredentialsStoreAction(this);
+    private final transient WebhookCredentialsStoreAction action = new WebhookCredentialsStoreAction(this);
 
-    private final ModelObject context;
+    /**
+     * List of credentials for serialization. This is used to store the credentials in the configuration file and is not used at runtime.
+     */
+    private List<IdCredentials> credentials = new ArrayList<>();
 
+    /**
+     * Constructor.
+     * @param provider the provider
+     * @param context the context
+     */
     public WebhookCredentialsStore(@NonNull WebhookCredentialsProvider provider, @NonNull ModelObject context) {
         super(WebhookCredentialsProvider.class);
         this.provider = provider;
-        this.context = context;
+        load();
     }
 
     @Override
     @NonNull
     public ModelObject getContext() {
-        return context;
+        return Jenkins.get();
     }
 
     @Override
@@ -88,6 +107,40 @@ public class WebhookCredentialsStore extends CredentialsStore {
     @CheckForNull
     public CredentialsStoreAction getStoreAction() {
         return action;
+    }
+
+    /**
+     * Saves the credentials to the configuration file.
+     * @throws IOException if an error occurs while saving the credentials
+     */
+    @Override
+    public synchronized void save() throws IOException {
+        this.credentials = new ArrayList<>(provider.getAllWebhookCredentials());
+        getConfigFile().write(this);
+    }
+
+    /**
+     * Loads the credentials from the configuration file.
+     */
+    protected synchronized void load() {
+        XmlFile xml = getConfigFile();
+        if (xml.exists()) {
+            try {
+                xml.unmarshal(this);
+                provider.setCredentials(this.credentials);
+            } catch (IOException e) {
+                LOG.error("Failed to load credentials from configuration file: {}", e.getMessage(), e);
+            }
+        }
+    }
+
+    /**
+     * Gets the configuration file that {@link WebhookCredentialsProvider} uses to store its credentials.
+     *
+     * @return the configuration file that {@link WebhookCredentialsProvider} uses to store its credentials.
+     */
+    public static XmlFile getConfigFile() {
+        return new XmlFile(Jenkins.XSTREAM2, new File(Jenkins.get().getRootDir(), "webhook-credentials.xml"));
     }
 
     /**
